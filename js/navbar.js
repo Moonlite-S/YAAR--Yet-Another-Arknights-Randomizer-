78dcb57125ace1fb23676a23c54123fa9db74533
Moonlite-Irony <iphoneagents00@gmail.com>
2023-09-21 16:07:16 -0500

    Initial Push

---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+var divs = ["Operator", "Levels", "RuleSettings"];
+var visibleTabs = null;
+
+function tabVisible(tab) {
+    if (visibleTabs == tab) 
+        visibleTabs = null;
+    else   
+        visibleTabs = tab;
+    hideOtherTabs();
+}
+
+function hideOtherTabs() {
+    var div;
+    for(t of divs) {
+        div = document.getElementById(t);
+        if (visibleTabs === t)
+            div.style.display = "block";
+        else
+            div.style.display = "none";
+    }
+}
